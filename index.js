4ca45aacaa85663b2fd314342ff2308c4f8dbf42
Maria Gabriele Soares <mariagabrielesoaresleite@gmail.com>
2020-12-11 06:30:29 +0000

    Atividade 01

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // Atividade 01
 function calculaMedia(notas) {
-  if( !Array.isArray(notas) || notas.length < 1 ) {
-    estouraExcecaoCustom(`Parâmetro inválido: ${notas}`);
+  var somaNotas = 0;
+
+  for(var nota of notas) {
+    somaNotas += nota;
   }
 
-  let notaTotal = 0;
-
-  for (const nota of notas) {
-    notaTotal += nota;
-  }
-
-  return notaTotal / notas.length;
+  var media = somaNotas / notas.length;
+  console.log(media);
 }
 
-// Função especial para evitar chamada incorreta
-function estouraExcecaoCustom(mensagem) {
-  throw new Error(mensagem);
-}
+calculaMedia([10, 5]);
